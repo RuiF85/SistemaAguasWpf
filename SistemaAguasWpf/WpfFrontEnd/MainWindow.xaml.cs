4cfe9f85504510b8bbbe79d31e227af185a62c69
RuiFernandes <Roy_f_5@hotmail.com>
2026-07-25 01:05:31 +0100

    Create dashboard interface

--- SistemaAguasWpf/WpfFrontEnd/MainWindow.xaml.cs
+++ SistemaAguasWpf/WpfFrontEnd/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Windows;
 using WpfFrontEnd.Services;
+using WpfFrontEnd.Views.Dashboard;
 
 namespace WpfFrontEnd
 {
@@ -10,20 +11,53 @@ namespace WpfFrontEnd
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ClienteApiService clienteService = new ClienteApiService();
+
 
         public MainWindow()
         {
             InitializeComponent();
 
-            CarregarClientes();
+            conteudoPrincipal.Content = new DashboardControl();
         }
 
-        private async void CarregarClientes()
+        private void BtnClientes_Click(object sender, RoutedEventArgs e)
         {
-            var clientes = await clienteService.ObterClientes();
-            
-            dgClientes.ItemsSource = clientes;
+            MessageBox.Show("Clientes");
+        }
+
+        private void BtnContadores_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Contadores");
+
+        }
+
+        private void BtnConsumos_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Consumos");
+
+        }
+
+        private void BtnFaturas_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Faturas");
+
+        }
+        
+        private void BtnSobre_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sobre");
+
+        }
+        private void BtnSair_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult resposta = MessageBox.Show("deseja sair da aplicação?","Sair",
+                MessageBoxButton.YesNo,MessageBoxImage.Question);
+
+            if (resposta == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+
         }
     }
 }
